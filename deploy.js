eaea7568cd4754728cff22323ebe96b1672dc1c6
samuelkenji <sa.kgs10@gmail.com>
2022-06-11 12:55:27 -0300

    first commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'present art stone jaguar aerobic there bounce skirt deny intact glide woman',
+  'https://rinkeby.infura.io/v3/db3d5ef98cfd4a5b994efa19b4c62013'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
